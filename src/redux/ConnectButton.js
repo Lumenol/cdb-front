cae0998347cdd89f18a89a71f76df92a72d42f33
llabourbeExcilys <llabourbe@excilys.com>
2019-06-18 16:16:55 +0200

    Fix state par défaut

--- src/redux/ConnectButton.js
+++ src/redux/ConnectButton.js
@@ -5,7 +5,11 @@ export function toggleConnect(isConnected){
     return {type: CONST_PRESS_CONNECT_BUTTON, isConnected}
 }
 
-export function RootReducer(state = false, action) {
+const initialState = {
+    isConnected: false
+};
+
+export function RootReducer(state = initialState, action) {
     if (action.type === CONST_PRESS_CONNECT_BUTTON) {
         return Object.assign({}, state, {
             isConnected: !state.isConnected
