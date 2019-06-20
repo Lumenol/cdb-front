07d07f12689c6b6b79474aeba7756da577946564
Louis-Maxime Crédeville <lcredeville@excilys.com>
2019-06-20 12:20:28 +0200

    action Login(async) => action set_token

--- src/redux/connection.js
+++ src/redux/connection.js
@@ -1,4 +1,18 @@
+import {getToken} from "../api/connection";
+
 const LOGOUT = "LOGOUT";
+const SET_TOKEN = "SET_TOKEN";
+
+export function login(username, password) {
+    return async function (dispatch) {
+        const token = await getToken(username, password);
+        dispatch(setToken(token));
+    }
+}
+
+function setToken(token) {
+    return {type: SET_TOKEN, token: token};
+}
 
 
 export function logout() {
@@ -9,6 +23,8 @@ export default function connectionReducer(state = '', action) {
     switch (action.type) {
         case LOGOUT:
             return '';
+        case SET_TOKEN:
+            return action.token;
         default:
             return state;
     }
