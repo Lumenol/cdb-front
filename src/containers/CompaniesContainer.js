bf7cd22a9ad3bc464e657173cf550e40de8fdba7
Dimitri3502 <dbeubry@gmail.com>
2019-06-25 18:02:32 +0200

    react CEST DE LA MERDE

--- src/containers/CompaniesContainer.js
+++ src/containers/CompaniesContainer.js
@@ -1,7 +1,6 @@
-import React, {forwardRef} from 'react';
+import React, {Component, forwardRef} from 'react';
 import MaterialTable from 'material-table';
-import {useTranslation} from "react-i18next";
-import {useSelector} from "react-redux";
+import {connect} from "react-redux";
 import {selectCompanies} from "../redux/selectors";
 
 import AddBox from '@material-ui/icons/AddBox';
@@ -20,50 +19,67 @@ import SaveAlt from '@material-ui/icons/SaveAlt';
 import Search from '@material-ui/icons/Search';
 import ViewColumn from '@material-ui/icons/ViewColumn';
 import {deleteCompany} from "../redux/companies";
+import {withTranslation} from "react-i18next";
 
-export function CompaniesContainer() {
-    const {t} = useTranslation();
 
-    const columns = [
-        {title: 'id', field: 'id'},
-        {title: 'Name', field: 'name'}
-    ];
-    const data = useSelector(selectCompanies);
+class CompaniesContainer extends Component {
 
-    const tableIcons = {
-        Add: forwardRef((props, ref) => <AddBox {...props} ref={ref}/>),
-        Check: forwardRef((props, ref) => <Check {...props} ref={ref}/>),
-        Clear: forwardRef((props, ref) => <Clear {...props} ref={ref}/>),
-        Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref}/>),
-        DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref}/>),
-        Edit: forwardRef((props, ref) => <Edit {...props} ref={ref}/>),
-        Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref}/>),
-        Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref}/>),
-        FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref}/>),
-        LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref}/>),
-        NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref}/>),
-        PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref}/>),
-        ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref}/>),
-        Search: forwardRef((props, ref) => <Search {...props} ref={ref}/>),
-        SortArrow: forwardRef((props, ref) => <ArrowUpward {...props} ref={ref}/>),
-        ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref}/>),
-        ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref}/>)
-    };
+    render() {
+        const columns = [
+            {title: 'id', field: 'id'},
+            {title: 'Name', field: 'name'}
+        ];
 
-    return (
-        <MaterialTable
-            icons={tableIcons}
-            title={t("companies")}
-            columns={columns}
-            data={data}
-            editable={{
-                onRowAdd: newData => {
-                },
-                onRowDelete: oldData => {
-                    return deleteCompany(oldData.id);
-                },
-            }}
-        />
-    );
 
+        const tableIcons = {
+            Add: forwardRef((props, ref) => <AddBox {...props} ref={ref}/>),
+            Check: forwardRef((props, ref) => <Check {...props} ref={ref}/>),
+            Clear: forwardRef((props, ref) => <Clear {...props} ref={ref}/>),
+            Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref}/>),
+            DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref}/>),
+            Edit: forwardRef((props, ref) => <Edit {...props} ref={ref}/>),
+            Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref}/>),
+            Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref}/>),
+            FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref}/>),
+            LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref}/>),
+            NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref}/>),
+            PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref}/>),
+            ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref}/>),
+            Search: forwardRef((props, ref) => <Search {...props} ref={ref}/>),
+            SortArrow: forwardRef((props, ref) => <ArrowUpward {...props} ref={ref}/>),
+            ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref}/>),
+            ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref}/>)
+        };
+        const {t, delete} = this.props;
+        const companies = this.props.companies;
+        return (
+            <MaterialTable
+                icons={tableIcons}
+                title={t("companies")}
+                columns={columns}
+                data={companies}
+                editable={{
+                    onRowAdd: newData => {
+                    },
+                    onRowDelete: delete
+
+                }}
+            />
+        );
+    }
 }
+
+const mapDispatchToProps = (dispatch, props) => {
+    return {
+        delete: () => dispatch(deleteCompany(props.data.id))
+    }
+};
+
+const mapStateToProps = (state) => {
+    return {
+        companies: selectCompanies(state)
+    };
+};
+
+const Translation = withTranslation()(CompaniesContainer);
+export default CompaniesContainer = connect(mapStateToProps, mapDispatchToProps)(Translation);
